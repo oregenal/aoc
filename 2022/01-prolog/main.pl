14c82c775c8eb762974b7c3400030a68cef98db2
Andrey Gud <nosacred@gmail.com>
2026-08-10 13:12:15 +0700

    AoC-2022 Day 1 Part 2 in prolog

--- 2022/01-prolog/main.pl
+++ 2022/01-prolog/main.pl
@@ -10,8 +10,7 @@ process_file(Fname, Result) :-
 process_data(Stream, Result) :-
 	stream_list(Stream, List),
 	process_input(List, SL),
-	maplist(colories, SL, RL),
-	find_most(RL, Result).
+	maplist(colories, SL, Result).
 
 stream_list(Stream, List) :-
 	read_string(Stream, _, Str),
@@ -25,23 +24,34 @@ process_input_in([H|Tail], Acc, [SL|Ntail]) :-
 
 colories(List, Lc) :- colories_in(List, 0, Lc).
 colories_in([], A, A) :- !.
-colories_in([H|Tail], A, Lc) :- string_length(H, 0), !, colories_in(Tail, A, Lc).
-colories_in([H|Tail], A, Lc) :- number_string(X, H), A1 is X + A, colories_in(Tail, A1, Lc).
+colories_in([H|Tail], A, Lc) :- 
+	string_length(H, 0), 
+	!, colories_in(Tail, A, Lc).
+colories_in([H|Tail], A, Lc) :- 
+	number_string(X, H), 
+	A1 is X + A, colories_in(Tail, A1, Lc).
 
-find_most(L, R) :- find_most_in(L, 0, R).
-find_most_in([], A, A) :- !.
-find_most_in([H|Tail], A, R) :-  
-	(H > A, !, find_most_in(Tail, H, R) 
-	; find_most_in(Tail, A, R)).
+find_most(L, R) :-
+	sort(0, @>=, L, SortL), SortL = [R|_].
+
+sum_3_most(L, R) :-
+	sort(0, @>=, L, SortL), SortL =[A1|[A2|[A3|_]]], R is A1 + A2 + A3.
+
+get_result(Fname) :-
+	process_file(Fname, List),
+	find_most(List, Result1),
+	sum_3_most(List, Result2),
+	format('~w, ~s:~w~n', [Fname, "Part1", Result1]),
+	format('~w, ~s:~w~n', [Fname, "Part2", Result2]).
 
 main :-
 	on_signal(int, _, default),
 	current_prolog_flag(argv, Argv),
-	(Argv = [Fname|_],
-		process_file(Fname, Result),
-		write(Fname),
-		write(":"),
-		write(Result);
+	(Argv = [Sample|_],
+		get_result(Sample);
+		write("Provide input file\n")),
+	(Argv = [_|[Input|_]],
+		get_result(Input);
 		write("Provide input file\n")).
 
 % vim: set filetype=prolog:
