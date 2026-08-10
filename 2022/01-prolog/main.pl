c99887b4e2677218480ebf0063031fea9393c8a4
Andrey Gud <nosacred@gmail.com>
2026-08-10 20:31:12 +0700

    some refactoring

--- 2022/01-prolog/main.pl
+++ 2022/01-prolog/main.pl
@@ -16,20 +16,20 @@ stream_list(Stream, List) :-
 	read_string(Stream, _, Str),
 	atomic_list_concat(List, '\n\n', Str).
 
-process_input(Input, List) :- process_input_in(Input, [], List).
-process_input_in([], Acc, Acc) :- !.
-process_input_in([H|Tail], Acc, [SL|Ntail]) :- 
+process_input(Input, List) :- process_input(Input, [], List).
+process_input([], Acc, Acc) :- !.
+process_input([H|Tail], Acc, [SL|Ntail]) :- 
 	split_string(H, "\n", "", SL),
-	process_input_in(Tail, Acc, Ntail).
+	process_input(Tail, Acc, Ntail).
 
-colories(List, Lc) :- colories_in(List, 0, Lc).
-colories_in([], A, A) :- !.
-colories_in([H|Tail], A, Lc) :- 
+colories(List, Lc) :- colories(List, 0, Lc).
+colories([], A, A) :- !.
+colories([H|Tail], A, Lc) :- 
 	string_length(H, 0), 
-	!, colories_in(Tail, A, Lc).
-colories_in([H|Tail], A, Lc) :- 
+	!, colories(Tail, A, Lc).
+colories([H|Tail], A, Lc) :- 
 	number_string(X, H), 
-	A1 is X + A, colories_in(Tail, A1, Lc).
+	A1 is X + A, colories(Tail, A1, Lc).
 
 find_most(L, R) :-
 	sort(0, @>=, L, SortL), SortL = [R|_].
